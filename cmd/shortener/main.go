7aa2c0479bea9daa117c804334e24cfa87f28fea
nikita.a.zaytsev <nikita.a.zaytsev@onefactor.com>
2025-09-28 23:03:49 +0300

    First iteration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,44 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	return http.ListenAndServe(`:8080`, http.HandlerFunc(webhook))
+}
+
+func webhook(res http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		if req.Header.Get("Content-Type") != "text/plain" {
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		body, _ := io.ReadAll(req.Body)
+		fmt.Println("Request body:")
+		fmt.Println(string(body))
+
+		res.Header().Set("Content-Type", "text/plain")
+		res.WriteHeader(http.StatusCreated)
+		res.Write([]byte("http://localhost:8080/EwHXdJfB"))
+	case http.MethodGet:
+		fmt.Println("Request path:")
+		fmt.Println(req.URL.Path)
+
+		res.Header().Set("Location", "http://localhost:8080/EwHXdJfB")
+		res.WriteHeader(http.StatusTemporaryRedirect)
+	default:
+		res.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JacksonGibsonESP/go-url-shortener
+
+go 1.24.5
