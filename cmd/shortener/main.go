a68e64660def693f9ff4620c18fe84aaa22be6f0
nikita.a.zaytsev <nikita.a.zaytsev@onefactor.com>
2025-09-29 10:07:05 +0300

    Simple shortening realization

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 )
 
@@ -16,6 +17,41 @@ func run() error {
 	return http.ListenAndServe(`:8080`, http.HandlerFunc(webhook))
 }
 
+func randomString(length int) string {
+	const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+
+var urlToShort map[string]string = make(map[string]string)
+var shortToUrl map[string]string = make(map[string]string)
+
+const shortUrlLength = 8
+
+func createShortUrl(url string) string {
+	shortUrl, ok := urlToShort[url]
+	if ok {
+		return shortUrl
+	} else {
+		shortUrl = "/" + randomString(shortUrlLength)
+		urlToShort[url] = shortUrl
+		shortToUrl[shortUrl] = url
+		return shortUrl
+	}
+}
+
+func getUrlByShort(short string) string {
+	url, ok := shortToUrl[short]
+	if ok {
+		return url
+	} else {
+		return ""
+	}
+}
+
 func webhook(res http.ResponseWriter, req *http.Request) {
 	switch req.Method {
 	case http.MethodPost:
@@ -25,17 +61,27 @@ func webhook(res http.ResponseWriter, req *http.Request) {
 		}
 
 		body, _ := io.ReadAll(req.Body)
-		fmt.Println("Request body:")
-		fmt.Println(string(body))
+		url := string(body)
+		fmt.Println("URL requested to short:")
+		fmt.Println(url)
+
+		shortUrl := createShortUrl(url)
 
 		res.Header().Set("Content-Type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte("http://localhost:8080/EwHXdJfB"))
+		res.Write([]byte("http://localhost:8080" + shortUrl))
 	case http.MethodGet:
-		fmt.Println("Request path:")
-		fmt.Println(req.URL.Path)
+		shortUrl := req.URL.Path
 
-		res.Header().Set("Location", "https://practicum.yandex.ru/")
+		fmt.Println("Short URL requested:")
+		fmt.Println(shortUrl)
+
+		url := getUrlByShort(shortUrl)
+
+		fmt.Println("URL found:")
+		fmt.Println(url)
+
+		res.Header().Set("Location", url)
 		res.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		res.WriteHeader(http.StatusBadRequest)
