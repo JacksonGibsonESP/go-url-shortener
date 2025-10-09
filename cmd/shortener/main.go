9a84a6061f608a769a4159605f09e52cc913d153
nikita.a.zaytsev <nikita.a.zaytsev@onefactor.com>
2025-10-09 17:11:33 +0300

    Environment variables

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"net/http"
+	"os"
 
 	"github.com/JacksonGibsonESP/go-url-shortener/internal/config"
 	"github.com/JacksonGibsonESP/go-url-shortener/internal/handler"
@@ -11,6 +12,16 @@ import (
 func main() {
 	config.Init()
 	flag.Parse()
+
+	value, isPresent := os.LookupEnv("SERVER_ADDRESS")
+	if isPresent {
+		config.Config.CurrentAdress = value
+	}
+
+	value, isPresent = os.LookupEnv("BASE_URL")
+	if isPresent {
+		config.Config.TargetAdress = value
+	}
 
 	router := handler.URLRouter()
 	http.ListenAndServe(config.Config.CurrentAdress, router)
